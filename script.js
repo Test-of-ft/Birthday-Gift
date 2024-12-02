e1eba40922ca257c7e2020da5c9ea2fb9ad49998
Biswajit <121815367+GabBiswajit@users.noreply.github.com>
2024-12-02 15:53:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function displayBirthday() {
-   const name = Biswajit;
-   const age = 16;
-   const dob = 4/12/2024;
-   const picture = 
+   const name = 'Biswajit';
+   const age = '16';
+   const dob = '21/3/2007';
+   const picture = 'https://media.discordapp.net/attachments/1197080451173593119/1313088094026731530/royal-boys-dp_76-576x1024.webp?ex=674edc15&is=674d8a95&hm=6ae8351dbb36b6dbfb287dc0345b8bedbd219e308131d800a46043c746a82390&';
       
    document.getElementById('birthdayName').textContent = `Today is ${name}'s Birthday`;
    document.getElementById('birthdayAge').textContent = `${age} years old`;
